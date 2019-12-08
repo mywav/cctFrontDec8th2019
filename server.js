2f57675c401b9dd6d325f10e6b42b21888373903
mywav <ryan_newbold@me.com>
2019-12-08 12:36:09 -0700

    another update to heroky deploy

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(express.static(__dirname + '/dist/cctFrontDec8th2019'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/cct/FrontDec8th2019/index.html'));
+res.sendFile(path.join(__dirname+'/dist/cctFrontDec8th2019/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
