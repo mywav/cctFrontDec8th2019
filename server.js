d5143472f846ec8c396448a1af026a3b33eb95fb
mywav <ryan_newbold@me.com>
2020-02-12 11:57:07 -0700

    CHANGED SERVER.JS TO MATCH WITHCALLBACK BRANCH

--- server.js
+++ server.js
@@ -1,16 +1,38 @@
 //Install express server
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const bodyParser = require('body-parser');
+const proxy = require('express-http-proxy');
+const cors = require('cors');
+
 
 const app = express();
 
-// Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/cctFrontDec8th2019'));
+// Parsers for POST data
+app.use(express.static(__dirname + '/dist'));
+app.use(bodyParser.json({limit: '20mb'}));
+app.use(bodyParser.urlencoded({ extended: false, limit: '20mb' }));
 
-app.get('/*', function(req,res) {
-    
-res.sendFile(path.join(__dirname+'/dist/cctFrontDec8th2019/index.html'));
+app.use(cors());
+
+//Point static path to dist
+app.use(express.static(path.join(__dirname, 'dist')));
+
+// Set out api routes proxy to point to spring boot server
+app.use('/server', proxy('http://localhost:8080'))
+
+// Catch all other routes and return the index file
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'dist/index.html'));
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+// Get port from environment and store in Express.
+const port = '4200';
+app.set('port', port);
+
+// Create HTTP server.
+const server = http.createServer(app);
+
+// Listen on provided port, on all network interfaces.
+server.listen(port, () => console.log('API running on ${port}'));
