b1091a4d9b5ff118cb97773a8363f077e3da4264
mywav <ryan_newbold@me.com>
2020-02-19 05:30:21 -0700

    ADDED CORS HEADER IN SERVER.JS

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ const cors = require('cors');
 
 const app = express();
 
+app.all('/', function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  next()
+});
 app.use(bodyParser.json({limit: '20mb'}));
 app.use(bodyParser.urlencoded({ extended: false, limit: '20mb' }));
 app.use(cors());
