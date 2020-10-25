82a580392253d5f3c4f09383d756137df7d1bba0
mywav <ryan_newbold@me.com>
2020-10-25 09:21:29 -0600

    Changed date format to 'MM-dd-yyyy'

--- server.js
+++ server.js
@@ -1,22 +1,23 @@
 // Get dependencies
-const express = require('express');
-const path = require('path');
-const http = require('http');
-const bodyParser = require('body-parser');
-const proxy = require('express-http-proxy');
-const cors = require('cors');
+const express = require("express");
+const path = require("path");
+const http = require("http");
+const bodyParser = require("body-parser");
+const proxy = require("express-http-proxy");
+const cors = require("cors");
 
 const app = express();
-app.use(bodyParser.json({limit: '20mb'}));
-app.use(bodyParser.urlencoded({ extended: false, limit: '20mb' }));
+app.use(bodyParser.json({ limit: "20mb" }));
+app.use(bodyParser.urlencoded({ extended: false, limit: "20mb" }));
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'dist/cctFrontDec8th2019')));
-app.use('/server', proxy('http://colorcrayontipbackend.ryannewbold.com'));
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist/cctFrontDec8th2019/index.html'));
+app.use(express.static(path.join(__dirname, "dist/cctFrontDec8th2019")));
+app.use("/server", proxy("http://colorcrayontipbackend.ryannewbold.com"));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist/cctFrontDec8th2019/index.html"));
 });
-const port = process.env.PORT;
-app.set('port', port);
+// const port = process.env.PORT;
+const port = 8080;
+app.set("port", port);
 
 const server = http.createServer(app);
 server.listen(port, () => console.log(`API running on ${port}`));
